2ed0d2b3b4a82aaa7d7a0fc660c0003b1e2c0b22
Jakub <kuborok123434@gmail.com>
2022-09-16 11:10:41 +0200

    send form data to example sheet

--- app/utils/spreadsheets.server.js
+++ app/utils/spreadsheets.server.js
@@ -2,6 +2,10 @@ import { getGoogleUtils } from "./google.server";
 
 const spreadsheetId = "1BnhE0HuBiqROZLKM1D5xZzYxXQvqINdDAfkemutQtVM";
 
+const convertDate = (date) => {
+  return date.split("-").reverse().join(".");
+};
+
 export const getRows = async () => {
   const { auth, googleSheets } = await getGoogleUtils();
   const rows = await googleSheets.spreadsheets.values.get({
@@ -13,8 +17,20 @@ export const getRows = async () => {
   console.log(rows.data.values);
 };
 
-export const writeRows = async () => {
+export const writeRows = async (formdata) => {
+  console.log(formdata);
+
   // Write row(s) to spreadsheet
+  const dataForSheet = [
+    formdata.formname,
+    `${formdata.firstname} ${formdata.lastname}`,
+    convertDate(formdata.startdate),
+    convertDate(formdata.enddate),
+    " ",
+    formdata.cartype,
+    formdata.hoteldata,
+  ];
+
   const { auth, googleSheets } = await getGoogleUtils();
   await googleSheets.spreadsheets.values.append({
     auth,
@@ -22,7 +38,7 @@ export const writeRows = async () => {
     range: "Arkusz1!A:B",
     valueInputOption: "USER_ENTERED",
     resource: {
-      values: [["First Test", "Second", "1/1/2020", "4", "4.5", "4,5"]],
+      values: [dataForSheet],
     },
   });
 };
